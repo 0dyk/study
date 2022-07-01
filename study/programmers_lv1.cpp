29c2c7a146d2d84ee71283c87aabc1c0c692b55a
zerodyk <oooo_0@naver.com>
2022-07-02 02:46:26 +0900

    0702

--- study/programmers_lv1.cpp
+++ study/programmers_lv1.cpp
@@ -316,7 +316,73 @@ string solution(string new_id) {
 
 /* lv1. 최소직사각형
 
+#include <string>
+#include <vector>
 
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+
+    int w = 0;
+    int h = 0;
+
+    for(int i = 0; i < sizes.size(); i++)
+    {
+        if(sizes[i][0] > sizes[i][1])
+        {
+            if(sizes[i][0] > w)
+            {
+                w = sizes[i][0];
+            }
+
+            if(sizes[i][1] > h)
+            {
+                h = sizes[i][1];
+            }
+        }
+        else
+        {
+            if(sizes[i][1] > w)
+            {
+                w = sizes[i][1];
+            }
+
+            if(sizes[i][0] > h)
+            {
+                h = sizes[i][0];
+            }
+        }
+    }
+
+    answer = w * h;
+
+    return answer;
+}
+
+alforithm의 max 와 min을 사용
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+
+    int w = 0, h = 0;
+
+    for(int i = 0; i < sizes.size(); ++i)
+    {
+        w = max(w, max(sizes[i][0], sizes[i][1]));
+        h = max(h, min(sizes[i][0], sizes[i][1]));
+    }
+
+    answer = w * h;
+
+    return answer;
+}
 
 */
 
